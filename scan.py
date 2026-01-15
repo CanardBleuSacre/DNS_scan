74e8dd6eeb1c3637fcb0a773abc7fc0a57424e58
Tom Delmaire <tom.delmaire@oteria.fr>
2026-01-15 11:58:38 +0100

    filtre les nouveaux domaines à ajouter et parcourir

--- scan.py
+++ scan.py
@@ -2,9 +2,18 @@ import sys
 import dns.resolver
 from rich.console import Console
 from rich.table import Table
+import re
 
 console = Console()
 record_types = ["A", "AAAA", "MX", "TXT"]
+
+DOMAIN_REGEX = (
+    rf"(?:[a-z0-9_]"
+    rf"(?:[a-z0-9-_]{{0,61}}"
+    rf"[a-z0-9_])?\.)"
+    r"+[a-z0-9][a-z0-9-_]{0,61}"
+    rf"[a-z]\.?"
+)
 
 if len(sys.argv) < 2:
     domain = input("Entrez un nom de domaine : ").strip()
@@ -27,11 +36,27 @@ while domains:
     for rtype in record_types:
         try:
             answers = dns.resolver.resolve(domain, rtype)
+            
             for rdata in answers:
+                text = rdata.to_text()
                 table.add_row(rtype, rdata.to_text())
-                new_domain = str(rdata).strip()
-                if new_domain not in visited and domain != new_domain:
-                    domains.append(new_domain)
+                
+                new_domains = set()
+
+                if rtype == "MX":
+                    parts = text.split()
+                    if len(parts) == 2:
+                        new_domains.add(parts[1].rstrip("."))
+
+                elif rtype == "TXT":
+                    found = re.findall(DOMAIN_REGEX, text, flags=re.IGNORECASE)
+                    for d in found:
+                        new_domains.add(d.rstrip("."))
+                
+                for nd in new_domains:
+                    if nd not in visited and nd != domain:
+                        domains.append(nd)
+        
         except dns.resolver.NoAnswer:
             table.add_row(rtype, "aucune donnée trouvée", style="red")
         except dns.resolver.NXDOMAIN:
@@ -40,6 +65,3 @@ while domains:
             table.add_row(rtype, f"erreur — {e}", style="red")
 
     console.print(table)
-
-
-#exemple d'utilisation : python adressipsearch.py mdk.fr google.com wikipedia.fr
