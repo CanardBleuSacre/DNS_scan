5f0e00fb46d44b5ba85c59c63fbe3838ebb1eb7f
Tom Delmaire <tom.delmaire@oteria.fr>
2026-01-21 19:58:09 +0100

    utilisation de argparse

--- scan.py
+++ scan.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from rich.tree import Tree
 from rich.progress import Progress, BarColumn, TextColumn, TimeElapsedColumn
@@ -6,24 +7,37 @@ from config import console
 from explorer import explore
 
 def main():
-    if len(sys.argv) < 2:
-        console.print("[bold red]Erreur:[/bold red] Nom de domaine manquant.")
-        console.print("[bold yellow]Usage:[/bold yellow] python scan.py [domain] [depth]")
-        exit(1)
-
-    start_domain = sys.argv[1].rstrip(".")
+    parser = argparse.ArgumentParser(
+        description="Outil de cartographie DNS écrit en Python",
+        epilog="Exemple : python scan.py oracle.com 1"
+    )
+    parser.add_argument(
+        "domain", 
+        help="Nom de domaine à scanner"
+    )
+    parser.add_argument(
+        "depth", 
+        type=int, 
+        nargs="?", 
+        default=2, 
+        help="Profondeur de récursion (défaut: 2)"
+    )
+    parser.add_argument(
+        "--version", 
+        action="version", 
+        version="DNS_scan 1.0"
+    )
+    args = parser.parse_args()
+    start_domain = args.domain.rstrip(".")
     
     if not start_domain or "." not in start_domain:
         console.print(f"[bold red]Erreur:[/bold red] '{start_domain}' n'est pas un domaine valide")
-        exit(1)
-    try:
-        max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 2
-        if max_depth < 1:
-            console.print(f"[bold red]Erreur:[/bold red] La profondeur ne peut pas être négative.")
-            exit(1)
-    except ValueError:
-        console.print(f"[bold red]Erreur:[/bold red] La profondeur doit être un nombre entier.")
-        exit(1)
+        sys.exit(1)
+
+    max_depth = args.depth
+    if max_depth < 1:
+        console.print(f"[bold red]Erreur:[/bold red] La profondeur ne peut pas être négative.")
+        sys.exit(1)
 
     root = Tree(f"[bold red]{start_domain}[/bold red]")
     with Progress(
@@ -32,9 +46,9 @@ def main():
         TextColumn("[bold]{task.completed}[/bold] domaines explorés"),
         TimeElapsedColumn()
     ) as progress:
-        task = progress.add_task("Initialisation...", total=None)
+        task = progress.add_task("Initialisation.. .", total=None)
         explore(start_domain, root, depth=0, max_depth=max_depth, progress=progress, task=task)
     console.print(root)
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
