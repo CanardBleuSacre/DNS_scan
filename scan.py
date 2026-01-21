aa91de83882af876b4a4880f6db2d134c27cf664
Tom Delmaire <tom.delmaire@oteria.fr>
2026-01-21 19:16:17 +0100

    amélioration des messages d'erreurs et ajout d'une validation des entrées

--- scan.py
+++ scan.py
@@ -7,11 +7,23 @@ from explorer import explore
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python scan.py [domain] [depth]")
+        console.print("[bold red]Erreur:[/bold red] Nom de domaine manquant.")
+        console.print("[bold yellow]Usage:[/bold yellow] python scan.py [domain] [depth]")
         exit(1)
 
     start_domain = sys.argv[1].rstrip(".")
-    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+    
+    if not start_domain or "." not in start_domain:
+        console.print(f"[bold red]Erreur:[/bold red] '{start_domain}' n'est pas un domaine valide")
+        exit(1)
+    try:
+        max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+        if max_depth < 1:
+            console.print(f"[bold red]Erreur:[/bold red] La profondeur ne peut pas être négative.")
+            exit(1)
+    except ValueError:
+        console.print(f"[bold red]Erreur:[/bold red] La profondeur doit être un nombre entier.")
+        exit(1)
 
     root = Tree(f"[bold red]{start_domain}[/bold red]")
     with Progress(
